1d51116067889b7398bd0a318c3593f2f67420de
Kiana Long <kianajade42.kl@gmail.com>
2021-04-06 18:23:56 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,7 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    3.times { @song.notes.build }
   end
 
   def create
@@ -50,4 +51,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title)
   end
 end
-
